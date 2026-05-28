29e6398599fbfc4a23348b087e64db213d563c09
a21371 <abuhler@sef.ca>
2026-05-28 10:41:55 -0400

    Delete netbox_fast_cable_v2 directory

--- netbox_fast_cable_v2/netbox_fast_cable/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from . import views
-
-app_name = "netbox_fast_cable"
-
-urlpatterns = [
-    path("", views.index, name="index"),
-    path("api/", include("netbox_fast_cable.api.urls")),
-]
